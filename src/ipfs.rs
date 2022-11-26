628fad91d80a70119b9811dda85e92ea4cb665ac
perama-v <83961755+perama-v@users.noreply.github.com>
2022-11-26 17:13:24 +1100

    Fix test imports

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -1,4 +1,5 @@
 //! IPFS-related helpers like CID computation.
+
 use anyhow::Result;
 
 use cid::multihash::{Code, MultihashDigest};
@@ -23,6 +24,8 @@ pub fn cid_v0_from_bytes(bytes: &[u8]) -> Result<Vec<u8>> {
 
 #[test]
 fn str_to_cidv0() {
+    use std::str::from_utf8;
+    
     let cid = cid_v0_from_bytes("beep boop".as_bytes()).unwrap();
     let string = from_utf8(&cid).unwrap();
     assert_eq!(string, "QmY6LjJ1HExi2TgHshc56ecPdVSNaWrzFbWq9sahHFrNoM");
@@ -30,6 +33,8 @@ fn str_to_cidv0() {
 
 #[test]
 fn str_to_cidv1() {
+    use std::str::from_utf8;
+
     let cid = cid_v1_from_bytes("beep boop".as_bytes()).unwrap();
     let string = from_utf8(&cid).unwrap();
     assert_eq!(
